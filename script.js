9d162bd7f1b093514d74fe09164b33334362be92
sahnoune aya <sahnouneaya15@gmail.com>
2024-03-31 00:18:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let bar = document.getElementById('bar');
+let nav = document.getElementById('nav');
+let close = document.getElementById('close')
+
+
+    bar.addEventListener('click', () => {
+      nav.style.right=0
+    })
+
+
+    close.addEventListener('click', () => {
+       nav.style.right="-200px"
+    })
